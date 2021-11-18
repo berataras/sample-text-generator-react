8f682f389d2275860c218a627b4c7580806e94c2
Berat Aras <berat.aras@hotmail.com>
2021-11-18 17:21:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import axios from 'axios'
 
 function App() {
+    const [text, setText] = useState('');
+    const [paragraph, setParagraph] = useState(4);
+    const [format, setFormat] = useState('text');
+
+    function fetchText(){
+        axios(`https://baconipsum.com/api/?type=all-meat&paras=${paragraph}&format=${format}`).then((response) => {
+            setText(response.data)
+        })
+    }
+    useEffect(() => {
+        fetchText()
+    }, [paragraph, format])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>React sample text generator app</h2>
+       <hr/>
+        <div className="form-container">
+          <div className="form-item">
+            <label>Paragraphs</label>
+            <input type="number" defaultValue={4} onChange={(e) => setParagraph(e.target.value)} className="form-control" />
+          </div>
+          <div className="form-item">
+            <label>Include HTML</label>
+            <select onChange={(e) => setFormat(e.target.value)} className="form-control">
+              <option selected value="text">No</option>
+              <option value="html">Yes</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="paragraph-container">
+            {text && text}
+        </div>
     </div>
   );
 }
